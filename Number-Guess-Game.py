6cd9ea871d7cb0e02d1df9bfc37f61f3e3eb6c86
Drizisokay <your-email@example.com>
2025-06-02 20:03:12 -0600

    Number Guessing Game.

---
+++ Number-Guess-Game.py
@@ -0,0 +1,9 @@
+'''
+Number guessing game
+Programmer: Daniel
+This project asks the user for a number and outputs a too low or too high. Limited attempts. 
+'''
+import random
+secret_number = random.num1(1,50)
+limited_attempts = 5 
+
